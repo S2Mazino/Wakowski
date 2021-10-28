5bdffbce82cff452d58345313ea73185dddf2e49
S2_Mazino <nordine.chaumath@hotmail.com>
2021-10-27 18:52:06 -0700

    added gateway intents

--- DiscordBot/src/main/java/DiscordBot/Bot.java
+++ DiscordBot/src/main/java/DiscordBot/Bot.java
@@ -4,6 +4,7 @@ import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.OnlineStatus;
 import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.requests.GatewayIntent;
 import net.dv8tion.jda.api.utils.ChunkingFilter;
 
 import javax.security.auth.login.LoginException;
@@ -22,7 +23,20 @@ public class Bot {
 		
 		//register JDA
 		JDA jda = JDABuilder
-				.createDefault(Config.get("token"))
+				.createDefault(Config.get("token")
+						,GatewayIntent.GUILD_MEMBERS
+						,GatewayIntent.GUILD_BANS
+						,GatewayIntent.GUILD_EMOJIS
+						,GatewayIntent.GUILD_WEBHOOKS
+						,GatewayIntent.GUILD_INVITES
+						,GatewayIntent.GUILD_VOICE_STATES
+						,GatewayIntent.GUILD_PRESENCES
+						,GatewayIntent.GUILD_MESSAGES
+						,GatewayIntent.GUILD_MESSAGE_REACTIONS
+						,GatewayIntent.GUILD_MESSAGE_TYPING
+						,GatewayIntent.DIRECT_MESSAGES
+						,GatewayIntent.DIRECT_MESSAGE_REACTIONS
+						,GatewayIntent.DIRECT_MESSAGE_TYPING)
 				.setChunkingFilter(ChunkingFilter.ALL)
 				.setStatus(OnlineStatus.ONLINE)
 				.setActivity(Activity.watching("you nooby"))
